c700baf4d1281b6f000c59d7c83d0b2483c253a3
Rishabh Gupta <rg171195@gmail.com>
2025-07-20 03:05:14 +0530

    fixing linting (flake8)
    fixing linting (flake8)

--- app/merriam_webster_test.py
+++ app/merriam_webster_test.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 import warnings
-warnings.filterwarnings("ignore", category=UserWarning, module="urllib3")
-warnings.filterwarnings("ignore", message="urllib3 v2 only supports OpenSSL 1.1.1+")
-warnings.filterwarnings("ignore", category=UserWarning, module="urllib3")
 import pytest
 import logging
 from merriam_webster import MerriamWebsterAPI
+
+# Suppress warnings
+warnings.filterwarnings("ignore", category=UserWarning, module="urllib3")
+warnings.filterwarnings(
+    "ignore", message="urllib3 v2 only supports OpenSSL 1.1.1+"
+    )
 
 # Initialize the API client for testing
 BASE_PATH = "/Users/rishabh.gupta01/Desktop/Merriam-Webster"
@@ -15,7 +18,9 @@ test_client = MerriamWebsterAPI(BASE_PATH)
 def test_fetch_definition():
     """Test cases for the MerriamWebsterAPI.fetch_definition method."""
     # Test with a valid word
-    assert test_client.fetch_definition("hospital") == "hospital (noun): a charitable institution for the needy, aged, infirm, or young"
+    assert test_client.fetch_definition("hospital") == (
+        "hospital (noun): a charitable institution for the needy, aged, infirm, or young"
+    )
 
     # Test with a nonexistent word
     assert test_client.fetch_definition("nonexistentword") == "Word not found"
@@ -33,8 +38,9 @@ def test_fetch_definition():
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    logging.info("Starting tests for MerriamWebsterAPI.fetch_definition method")
+    logging.info(" Testing MerriamWebsterAPI.fetch_definition method")
     pytest.main(["-v", __file__])
     logging.info("Tests completed.")
     logging.shutdown()
     print("Tests completed successfully.")
+# Add a blank line here
